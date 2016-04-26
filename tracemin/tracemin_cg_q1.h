a2106e8042990f3912c0ba872ef29807b2eb950b
yeungyh <yeungyh@gmail.com>
2016-04-26 01:51:40 -0400

    renamed:    tracemin_qr_cg.h -> tracemin_cg_q1.h

--- tracemin/tracemin_cg_q1.h
+++ tracemin/tracemin_cg_q1.h
@@ -16,11 +16,7 @@ class ProjectedMatrix
 		Mat Q1_;
 };
 
-int tracemin_cg( Mat A, Mat X, Mat BY, Mat AY, PetscInt M, PetscInt N);
-<<<<<<< HEAD:tracemin/tracemin_cg_q1.h
-=======
-int getQ1      ( Mat A, PetscInt M, PetscInt N); 
 
->>>>>>> 3e7d753d9f3c4a8ff6c295b2fc765fefe4116289:tracemin/tracemin_qr_cg.h
+int tracemin_cg( Mat A, Mat X, Mat BY, Mat AY, PetscInt M, PetscInt N);
 
 #endif // TRACE_MIN_CG
