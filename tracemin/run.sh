dd02e4a6498f7e9569338bc40fff1db8e382f054
yeungyh <yeungyh@gmail.com>
2016-04-27 20:50:30 -0400

    Added Deflation

--- tracemin/run.sh
+++ tracemin/run.sh
@@ -1,3 +1,7 @@
 #!/bin/tcsh
 
-./tracemin -fA ./../testcases/A_tiny.mtx -fB ./../testcases/B_tiny.mtx -fO eigen_cg.res
+if ($#argv == 0) then
+  ./tracemin -fA data/A_tiny.mtx -fB data/B_tiny.mtx -fO eigen_minres.res
+else if ($1 == "d") then
+  ./tracemin_deflation -fA data/A_tiny.mtx -fB data/B_tiny.mtx -fO eigen_minres_d.res
+endif
